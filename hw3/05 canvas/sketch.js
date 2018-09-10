a7f81d79a458334f51ed23bcb3c283c17e94c353
Mary Primeau <mary.primeau@uconnect.umt.edu>
2018-09-09 20:28:41 -0600

    learned about cavas and background

---
+++ hw3/05 canvas/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+    // first we need to specify our canvas size
+    createCanvas( 800, 400 );
+
+    // Then we can color this background
+    // Notice how the word 'blue' is surrounded in quotes
+    background('orange');
+    // Try replacing the word 'blue' with other common color names.
+    // NOTE: Be sure to keep the word surrounded with quotes.
